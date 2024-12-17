1d2382c6a07742cfd9af826b41099837a4be5d27
DanuKirilov <fazygoldmen@gmail.com>
2024-12-17 22:39:21 +0200

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { // Вводим номер месяца
+            Console.Write("Введите номер месяца (от 1 до 12): ");
+            int month = int.Parse(Console.ReadLine());
+
+            // Используем switch для определения месяца
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Январь");
+                    break;
+                case 2:
+                    Console.WriteLine("Февраль");
+                    break;
+                case 3:
+                    Console.WriteLine("Март");
+                    break;
+                case 4:
+                    Console.WriteLine("Апрель");
+                    break;
+                case 5:
+                    Console.WriteLine("Май");
+                    break;
+                case 6:
+                    Console.WriteLine("Июнь");
+                    break;
+                case 7:
+                    Console.WriteLine("Июль");
+                    break;
+                case 8:
+                    Console.WriteLine("Август");
+                    break;
+                case 9:
+                    Console.WriteLine("Сентябрь");
+                    break;
+                case 10:
+                    Console.WriteLine("Октябрь");
+                    break;
+                case 11:
+                    Console.WriteLine("Ноябрь");
+                    break;
+                case 12:
+                    Console.WriteLine("Декабрь");
+                    break;
+                default:
+                    Console.WriteLine("Неверный номер месяца. Введите число от 1 до 12.");
+                    break;
+            }
+
+            // Ожидаем нажатия клавиши перед закрытием программы
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}         
