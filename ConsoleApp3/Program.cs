62039889c7b27b0e01d038236d19487382f2ff56
DanuKirilov <fazygoldmen@gmail.com>
2024-12-17 22:42:14 +0200

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {// Вводим номер месяца
+            Console.Write("Введите номер месяца (от 1 до 12): ");
+            int month = int.Parse(Console.ReadLine());
+
+            // Используем switch для определения времени года
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Зима");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Весна");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Лето");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Осень");
+                    break;
+                default:
+                    Console.WriteLine("Неверный номер месяца. Введите число от 1 до 12.");
+                    break;
+            }
+
+            // Ожидаем нажатия клавиши перед закрытием программы
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
+    
+
